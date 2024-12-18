98d7dd1d4d3844691af63a7b7b7ed43ead4b7dd3
muskiteer <vansh.agarwal3110@gmail.com>
2024-12-19 00:52:57 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,21 @@
+let identity = document.getElementById("name");
+const phone = document.getElementById("phone");
+let button = document.getElementById("button"); 
+
+button.onclick = function() {
+    if (typeof identity.value !== 'string') {
+        alert("Please enter a valid name");
+        
+    } 
+    else if (typeof phone.value !== 'number') { // This regex checks if the phone number contains only digits
+        alert("Please enter a valid phone number");
+        
+    } 
+    else {
+        // alert("Your application has been submitted");
+        
+    }
+    window.location.href="form.html" ;
+};
+
+
